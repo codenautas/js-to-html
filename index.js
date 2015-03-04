ee0c3831a5f66808c205e406c07a28604e2c8172
Emilio <emilioplatzer@gmail.com>
2015-03-04 11:18:58 -0300

    Cambiando el Módulo a que tenga un objeto Html intermedio y que haya una función para cada tag.

--- index.js
+++ index.js
@@ -10,8 +10,43 @@
 
 "use strict";
  
-function jsToHtml(object){
-    return new jsToHtml.Internal(object);
+var jsToHtml={};
+
+jsToHtml.Html=function Html(directObject){
+    for(var property in directObject){
+        this[property]=directObject[property];
+    }
+}
+
+jsToHtml.Html.prototype.toHtmlText=function toHtmlText(){
+    console.log('render con ',this);
+    if('textNode' in this){
+        return this.textNode;
+    }
+    return "<"+this.tagName+
+        /*(object.attributes?Object.keys(object.attributes).map(function(attrName){
+            return ' '+attrName+'='+object.attributes[attrName];
+        }).join(''):'')+*/
+        ">"+
+        /*
+        (opts.pretty?spaces(recurseOpts.margin):'')+
+        (object.content?nl+object.content.map(function(node){
+            // esto hay que controlarlo cuando permitamos meter objetos internos dentro de objetos comunes para pasarlos a internos otra vez. 
+            // if(!(node instanceof jsToHtml.Internal)){
+                node=jsToHtml(node);
+            //}
+            return node.toHtml(opts,{margin:recurseOpts.margin+2});
+        }).join('')+(opts.pretty?spaces(recurseOpts.margin):''):'')+
+        (object.textContent||'')+
+        */
+        "</"+this.tagName+">"//+nl;
+}
+
+jsToHtml.direct=function direct(directObject){
+    return new jsToHtml.Html(directObject);
+}
+
+jsToHtml.html=function html(){
 }
 
 jsToHtml.Internal=function(object){
@@ -28,22 +63,7 @@ jsToHtml.Internal.prototype.toHtml=function toHtml(opts,recurseOpts){
     recurseOpts=recurseOpts||{};
     recurseOpts.margin=recurseOpts.margin||0;
     var nl=(opts.pretty?'\n':'');
-    return (opts.pretty?spaces(recurseOpts.margin):'')+
-        "<"+object.tagName+
-        (object.attributes?Object.keys(object.attributes).map(function(attrName){
-            return ' '+attrName+'='+object.attributes[attrName];
-        }).join(''):'')+
-        ">"+
-        (object.content?nl+object.content.map(function(node){
-            // esto hay que controlarlo cuando permitamos meter objetos internos dentro de objetos comunes para pasarlos a internos otra vez. 
-            // if(!(node instanceof jsToHtml.Internal)){
-                node=jsToHtml(node);
-            //}
-            return node.toHtml(opts,{margin:recurseOpts.margin+2});
-        }).join('')+(opts.pretty?spaces(recurseOpts.margin):''):'')+
-        (object.textContent||'')+
-        "</"+object.tagName+">"+nl;
-}
+    return }
 
 Object.defineProperty(jsToHtml.Internal.prototype, 'internal', {
     get: function(){
--- test/test.js
+++ test/test.js
@@ -1,29 +1,76 @@
 "use strict";
 
 var expect = require('expect.js');
-var jh = require('..');
+var jsToHtml = require('..');
 
 describe('js-to-html', function(){
     describe('basic test', function(){
-        it('should render an element with textContent', function(){
-            var object=jh({
+        var html = jsToHtml.html;
+        var direct = jsToHtml.direct;
+        beforeEach(function(){
+        });
+        it('should render a simple text', function(){
+            var textNode=direct({textNode:"simple text"});
+            expect(textNode).to.be.a(jsToHtml.Html);
+            var htmlText=textNode.toHtmlText();
+            expect(htmlText).to.eql("simple text");
+        });
+        it('should render a simple empty text', function(){
+            var textNode=direct({textNode:""});
+            expect(textNode).to.be.a(jsToHtml.Html);
+            var htmlText=textNode.toHtmlText();
+            expect(htmlText).to.eql("");
+        });
+        it('should render an element without content', function(){
+            var div=direct({
+                tagName:'div',
+                attributte:{},
+                content:[]
+            });
+            expect(div).to.be.a(jsToHtml.Html);
+            var htmlText=div.toHtmlText();
+            expect(htmlText).to.eql("<div></div>");
+        });
+        it('should control the presence and type/class of all properties', function(){
+            expect(function(){
+                direct({tagName:'div', attributte:{}, content:"must not be a string"})
+            }).to.trowError(/must include content of Array class/);
+            expect(function(){
+                direct({tagName:'div', content:[], attributes:['must not be an array']})
+            }).to.trowError(/must include attributes of Object class/);
+            expect(function(){
+                direct({tagName:8, content:[], attributes:{valid:true}});
+            }).to.trowError(/must include tagName of string type/);
+        });
+        it.skip('should render an element without content', function(){
+            var p=direct({
+                tagName:'p',
+                attributte:{},
+                content:'The first example'
+            });
+            expect(p).to.be.a(jsToHtml.Html);
+            var htmlText=p.toHtmlText();
+            expect(htmlText).to.eql("<p>The first example</p>");
+        });
+        it.skip('should render an element with textContent', function(){
+            var p=html.p('The first example');
+            expect(p).to.be.a(jsToHtml.Html);
+            expect(p.toDumpObject()).to.eql({
                 tagName:'p',
                 textContent:'The first example'
             });
-            expect(object).to.be.a(jh.Internal);
-            var htmlText=object.toHtml();
+            var htmlText=p.toHtmlText();
             expect(htmlText).to.eql("<p>The first example</p>");
         });
-        it('should render a span with attributes', function(){
-            expect(jh({
-                tagName:'span',
-                attributes:{'class':'the_class', id:'44'},
-                textContent:'The second example'
-            }).toHtml()).to.eql(
+        it.skip('should render a span with attributes', function(){
+            expect(html.span(
+                {'class':'the_class', id:'44'},
+                'The second example'
+            ).toHtml()).to.eql(
                 "<span class=the_class id=44>The second example</span>"
             );
         });
-        it('should render a div with other elements inside', function(){
+        it.skip('should render a div with other elements inside', function(){
             expect(jh({
                 tagName:'div',
                 attributes:{'class':'the_class', id:'47'},
@@ -38,7 +85,7 @@ describe('js-to-html', function(){
                 "</div>"
             );
         });
-        it('should render a div with other elements inside in a pretty way', function(){
+        it.skip('should render a div with other elements inside in a pretty way', function(){
             expect(jh({
                 tagName:'div',
                 attributes:{'class':'the_class', id:'47'},
@@ -53,7 +100,7 @@ describe('js-to-html', function(){
                 "    </div>\n"
             );
         });
-        it('should expose the internal content for test purposes',function(){
+        it.skip('should expose the internal content for test purposes',function(){
             var object={
                 tagName:'div',
                 attributes:{'class':'the_class', id:'47'},
@@ -65,7 +112,7 @@ describe('js-to-html', function(){
             jh.testing=true;
             expect(jh(object).internal).to.eql(object);
         });
-        it('should not expose the internal content if not for test purposes',function(){
+        it.skip('should not expose the internal content if not for test purposes',function(){
             jh.testing=false;
             expect(function(){
                 jh({tagName:'div'}).internal
