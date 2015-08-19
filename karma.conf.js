aa20c38563172233d0ae3c80edc7b482d91be346
Emilio <emilioplatzer@gmail.com>
2015-08-19 15:22:38 -0300

    adaptación a qa-control

--- karma.conf.js
+++ karma.conf.js
@@ -68,5 +68,5 @@ module.exports = function(config) {
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: !!process.env.TRAVIS
-  })
-}
+  });
+};
